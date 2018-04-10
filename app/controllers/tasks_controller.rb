fb2c00bd1b23b9139fcd6eaadb5add9566bf10cf
Arit Amana <>
2018-04-10 12:35:55 -0400

    Loading the Tasks onto the Page - JQuery

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   def index
-    render json: Task.all
+    render json: Task.all # the controller won't seek an index.html.erb file to render;
+                          # it's rendering the data in json format
   end
 end
