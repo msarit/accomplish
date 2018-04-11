3e3ad2e6994d603c01772d9edd53d3b63c96c6a8
Arit Amana <>
2018-04-10 20:09:50 -0400

    Make order of tasks consistent

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,26 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  # describe "tasks#index" do
-  #   it "should list the tasks in the database" do
-  #     task1 = FactoryBot.create(:task)
-  #     task2 = FactoryBot.create(:task)
-  #     get :index
-  #     expect(response).to have_http_status :success
-
-  #     response_value = ActiveSupport::JSON.decode(@response.body)
-  #     expect(response_value.count).to eq(2) # still works without brackets
-  #   end
-  # end
-
-  describe "tasks#update" do
-    it "should allow tasks to be marked as done" do
-      task = FactoryBot.create(:task, done: false)
-      put :update, params: {id: task.id, task: { done: true }}
+  describe "tasks#index" do
+    it "should list the tasks in the database" do
+      task1 = FactoryBot.create(:task)
+      task2 = FactoryBot.create(:task)
+      task1.update_attributes(title: "Something else")
+      get :index
       expect(response).to have_http_status :success
 
-      task.reload
-      expect(task.done).to eq true
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value.count).to eq(2) # still works without brackets
+      response_ids = response_value.collect do |task|
+        task["id"]
+      end
+      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
+
+  # describe "tasks#update" do
+  #   it "should allow tasks to be marked as done" do
+  #     task = FactoryBot.create(:task, done: false)
+  #     put :update, params: {id: task.id, task: { done: true }}
+  #     expect(response).to have_http_status :success
+
+  #     task.reload
+  #     expect(task.done).to eq true
+  #   end
+  # end
 end
