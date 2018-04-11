e39e02045bec94f099f2140da819b345e0760564
Arit Amana <>
2018-04-11 11:34:58 -0400

    Add Task Creation to API

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  
   def index
     render json: Task.order(:id) # the controller won't seek an index.html.erb file to render;
                           # it's rendering the data in json format
@@ -11,9 +12,14 @@ class TasksController < ApplicationController
     render json: task
   end
 
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:title, :done)
   end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,22 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  describe "tasks#index" do
-    it "should list the tasks in the database" do
-      task1 = FactoryBot.create(:task)
-      task2 = FactoryBot.create(:task)
-      task1.update_attributes(title: "Something else")
-      get :index
-      expect(response).to have_http_status :success
+
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: { title: "Feed Baby" }}
+      expect(response).to have_http_status(:success)
 
       response_value = ActiveSupport::JSON.decode(@response.body)
-      expect(response_value.count).to eq(2) # still works without brackets
-      response_ids = response_value.collect do |task|
-        task["id"]
-      end
-      expect(response_ids).to eq([task1.id, task2.id])
+      expect(response_value['title']). to eq("Feed Baby")
+      expect(Task.last.title).to eq("Feed Baby")
     end
   end
+
+  # describe "tasks#index" do
+  #   it "should list the tasks in the database" do
+  #     task1 = FactoryBot.create(:task)
+  #     task2 = FactoryBot.create(:task)
+  #     task1.update_attributes(title: "Something else")
+  #     get :index
+  #     expect(response).to have_http_status :success
+
+  #     response_value = ActiveSupport::JSON.decode(@response.body)
+  #     expect(response_value.count).to eq(2) # still works without brackets
+  #     response_ids = response_value.collect do |task|
+  #       task["id"]
+  #     end
+  #     expect(response_ids).to eq([task1.id, task2.id])
+  #   end
+  # end
 
   # describe "tasks#update" do
   #   it "should allow tasks to be marked as done" do
